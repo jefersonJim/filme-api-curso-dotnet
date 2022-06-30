5c1d1e897f60b2e1af11adb1faa1c73727a660e3
Jeferson Macedo <jefersonmacedo@ajtec.pt>
2022-06-30 12:32:21 +0100

    feat: Criar Token service

---
+++ UsuariosApi/Models/Token.cs
@@ -0,0 +1,12 @@
+﻿namespace UsuariosApi.Models
+{
+    public class Token
+    {
+        public Token(string value)
+        {
+            this.value = value;
+        }
+
+        public string value { get; }
+    }
+}
